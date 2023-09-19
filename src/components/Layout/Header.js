a49036a061cda952cd818d28f1dac83e3d121d45
vishalnanania <vishal.nanania@gmail.com>
2023-09-18 21:19:55 -0400

    added header components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
+import HeaderCartBtn from './HeaderCartBtn';
+
+function Header(props) {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <HeaderCartBtn />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="food table"></img>
+            </div>
+        </React.Fragment>
+    );
+}
+export default Header;
---
+++ src/components/Layout/HeaderCartBtn.js
@@ -0,0 +1,20 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from './HeaderCartBtn.module.css';
+
+function HeaderCartBtn(props) {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>
+                Your Cart
+            </span>
+            <span className={classes.badge}>
+                3
+            </span>
+        </button>
+    );
+}
+export default HeaderCartBtn;
